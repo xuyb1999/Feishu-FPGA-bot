ac52a846c6f9265af2e6e8efe8ee62c7314f5ca4
xuyb1999 <xuyibin17@mails.ucas.ac.cn>
2024-04-27 13:32:00 +0800

    feat: create a naive hello-world bot

---
+++ main.py
@@ -0,0 +1,56 @@
+import argparse
+import requests
+import time
+import hmac
+import hashlib
+import base64
+
+class FeiShuRobot:
+   def __init__(self, robot_id, secret) -> None:
+       self.robot_id = robot_id
+       self.secret = secret
+
+   def gen_sign(self):
+       # Concatenate timestamp and secret
+       timestamp = int(round(time.time()))
+       string_to_sign = '{}\n{}'.format(timestamp, self.secret)
+       hmac_code = hmac.new(string_to_sign.encode("utf-8"), digestmod=hashlib.sha256).digest()
+
+       # Process the result via base64
+       sign = base64.b64encode(hmac_code).decode('utf-8')
+       return str(timestamp), str(sign)
+
+   def send_text(self, text):
+       try:
+           url = f"https://open.feishu.cn/open-apis/bot/v2/hook/{self.robot_id}"
+           headers = {"Content-Type": "text/plain"}
+
+           timestamp, sign = self.gen_sign()
+           data = {
+                    "timestamp": timestamp,
+                    "sign": sign,
+                    "msg_type": "text",
+                    "content": {
+                        "text": text
+                    }
+                }
+           r = requests.post(url, headers=headers, json=data)
+           print("发送飞书成功")
+
+           return r.text
+       except Exception as e:
+           print("发送飞书失败:", e)
+
+if __name__ == '__main__':
+    # Parse the input arguments
+    parser = argparse.ArgumentParser(description='Run this script to launch a Feishu robot to monitor FPGA status')
+    parser.add_argument('-i', '--id', type=str, help='The ID of Feishu robot', required=True)
+    parser.add_argument('-s', '--secret', type=str, help='The secret key of Feishu robot', required=True)
+
+    args = parser.parse_args()
+    robot_id = args.id
+    secret = args.secret
+
+    # Initialize the Feishu robot
+    feishu = FeiShuRobot(robot_id, secret)
+    feishu.send_text("Hello Feishu!")
